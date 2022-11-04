7b6f9c2734b828996f674938988dc2196745447f
harrisimon <harrisimon@gmail.com>
2022-11-04 14:45:56 -0400

    adjusted app name in nav

--- src/components/shared/Header.js
+++ src/components/shared/Header.js
@@ -56,7 +56,7 @@ const Header = ({ user }) => (
 	<Navbar bg='primary' variant='dark' expand='md'>
 		<Navbar.Brand>
             <Link to='/' style={linkStyle}>
-                react-auth-template
+                Candy Emporium🍬
             </Link>
         </Navbar.Brand>
 		<Navbar.Toggle aria-controls='basic-navbar-nav' />
